e91c509fc8214dce69fff056131e79e060547c61
monocoto3000 <223238@ids.upchiapas.edu.mx>
2023-05-27 12:01:59 -0600

    chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,8 +4,10 @@ import '../css/Chat.css'
 function Chat() {
   return (
     <div className='ChatContainer'>
-        
-
+      <div className='ChatFondo'>
+      </div>
+      <input className='ChatComentario'>
+      </input>
     </div>
   )
 }
