8e2beef34b338626d2174b92fcf547ef687f9596
monocoto3000 <223238@ids.upchiapas.edu.mx>
2023-05-27 12:32:20 -0600

    comentarios

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,12 +3,31 @@ import '../css/Chat.css'
 
 function Chat() {
   return (
-    <div className='ChatContainer'>
-      <div className='ChatFondo'>
+    <>
+      <div className='ChatContainer'>
+        <div className='SesionContainer'>
+            <button className='ChatSesion'>
+              Iniciar sesion
+            </button>
+        </div>
+        <div className='ChatFondo'>
+          <ul className='ChatComentarioIn'>
+            <li className='Comentario1'>
+            </li>
+            <li className='Comentario2'>
+            </li>
+            <li className='Comentario3'>
+            </li>
+            <li className='Comentario4'>
+            </li>
+            <li className='Comentario5'>
+            </li>
+          </ul>
+        </div>
+        <input className='ChatComentario'>
+        </input>
       </div>
-      <input className='ChatComentario'>
-      </input>
-    </div>
+    </>
   )
 }
 
