23875aa524e2b3c361b893fa384b060b998c58da
Rextro <223275@ids.upchiapas.edu.mx>
2023-05-27 11:34:25 -0600

    card Canciones

--- src/components/BarraNavegadora.jsx
+++ src/components/BarraNavegadora.jsx
@@ -1,10 +1,11 @@
-
+import CardCancion from './Cancion'
 import '../css/BarraNavegadora.css'
 
 function BarraNavegadora() {
   return (
     <div className='BarraNavegadoraContainer'>
         <p>BarradeNavegacion</p>
+        <CardCancion/>
     </div>
   )
 }
--- src/components/Cancion.jsx
+++ src/components/Cancion.jsx
@@ -1,4 +1,41 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
-export default function cardCancion(){
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
     
+  </Box>
+);
+
+const card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography variant="h5" component="div">
+        {bull}
+      </Typography>
+      <Typography variant="body2">
+        
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button variant="filled" startIcon={<PlayArrowIcon />}>Reproducir</Button>
+    </CardActions>
+  </React.Fragment>
+);
+
+export default function OutlinedCard() {
+  return (
+    <Box sx={{ minWidth: 275 }}>
+      <Card variant="outlined">{card}</Card>
+    </Box>
+  );
 }
